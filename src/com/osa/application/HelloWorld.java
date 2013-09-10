4ef5a20b96e5094a168a74c1dcb880d7b8337753
mdmedley <mdmeds@gmail.com>
2013-09-10 17:32:28 -0400

    Push to GitHub trigger test

--- src/com/osa/application/HelloWorld.java
+++ src/com/osa/application/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World...");
 		System.out.println("California knows how to party!");
 		System.out.println("In the city...city of Compton!!");
 		System.out.println(getMessage());
